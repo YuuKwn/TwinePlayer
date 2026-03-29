125ae145e9b434214eb24106f28b9b92ac18c552
YuuKwn <36296754+YuuKwn@users.noreply.github.com>
2026-03-29 15:21:12 -0300

    Revert "feat(illustrator): expose openFolder + updated generatePrompt signature in preload"
    This reverts commit eb552593405b8fe2f2008b89ba7a11eeac1db335.

--- preload.js
+++ preload.js
@@ -13,9 +13,8 @@ contextBridge.exposeInMainWorld('electronAPI', {
 // without touching electronAPI or breaking any existing functionality.
 contextBridge.exposeInMainWorld('illustratorAPI', {
     ensureOutputDir: (gamePath) => ipcRenderer.invoke('illustrator:ensure-output-dir', gamePath),
-    generatePrompt: (params) => ipcRenderer.invoke('illustrator:generate-prompt', params),
+    generatePrompt: (sceneText) => ipcRenderer.invoke('illustrator:generate-prompt', sceneText),
     queueComfyUI: (params) => ipcRenderer.invoke('illustrator:queue-comfyui', params),
     pollImage: (params) => ipcRenderer.invoke('illustrator:poll-image', params),
-    openFolder: (dirPath) => ipcRenderer.invoke('illustrator:open-folder', dirPath),
 });
 // --- End Illustrator Feature ---
